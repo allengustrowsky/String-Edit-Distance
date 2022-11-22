5a39278a66d770ec03021b84857a1046e164ee0e
Allen Gustrowsky <allen@may21.us>
2022-11-22 06:29:16 -0500

    Initialized first row/column of matrix

--- editDistance.cpp
+++ editDistance.cpp
@@ -17,14 +17,26 @@ int main()
 
         int matrix[(int)str_from.length() + 1][(int)str_to.length() + 1];
 
-        //
+        // initialize first row
+        // thanks w3schools for showing how to get array length below
+        for (int i = 0; i < (int)(sizeof(matrix[0]) / sizeof(int)); i++) {
+            matrix[0][i] = i;
+        }
+        // initialize first column
+        for (int i = 1; i < (int)(sizeof(matrix) / sizeof(matrix[0])); i++) {
+            matrix[i][0] = i;
+        }
+
+        // DELETE ME, loops thru all array indices
         for (int i = 0; i < ((int)str_from.length() + 1); i++)
         {
             for (int j = 0; j < ((int)str_to.length() + 1); j++)
             {
-                matrix[i][j] = -1;
-                std::cout << "matrix [" << i << "][" << j <<"]: " << matrix[i][j] << endl;
+                // matrix[i][j] = -1;
+                // std::cout << "matrix [" << i << "][" << j <<"]: " << matrix[i][j] << "  |  ";
+                std::cout << matrix[i][j] << "  ";
             }
+            std::cout << endl;
         }
 
         // fetch first string for loop
